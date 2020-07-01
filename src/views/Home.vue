c83d5061b09aefce3db23f473fe5754fdcafc02f
tcl_tcl <1097607852@qq.com>
2020-07-01 16:16:18 +0800

    准备发布的版本出现了bug了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div>这是v1.0.0添加上去的补丁哈哈哈哈</div>
     <div>这是v1.0.1添加上去的补丁测试下</div>
     <div class="txt">5555</div>
-
+    <div>准备发布的版本出现了bug了</div>
     <div class="txt" @click="devClick">重新修改下试试哈</div>
     <div>又是一个新的功能的搞定了的</div>
   </div>
