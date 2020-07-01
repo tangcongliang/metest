d78ce82d41b69c46f79888ddc20d92ded03f2a69
tcl_tcl <1097607852@qq.com>
2020-07-01 15:55:29 +0800

    dev新的功能开发完成了

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,20 @@
     <div>这是v1.0.0添加上去的补丁哈哈哈哈</div>
     <div>这是v1.0.1添加上去的补丁测试下</div>
     <div class="txt">5555</div>
+
+    <div class="txt" @click="devClick">重新修改下试试哈</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {}
+  components: {},
+  methods: {
+    devClick() {
+      console.log("55555");
+    }
+  }
 };
 </script>
 <style lang="less" >
